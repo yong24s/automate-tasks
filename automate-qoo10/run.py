5d81af4d8cb80f0721e37fd15601e08d5256de6f
Yong Hao TENG <yonghao.teng@gmail.com>
2019-04-17 18:51:44 +0100

    Use system's chromium-browser for compatibility with arm devices:

--- automate-qoo10/run.py
+++ automate-qoo10/run.py
@@ -10,35 +10,36 @@ async def get_credentials():
 
     return credentials
 
-async def do_daily(credentials):
+async def do_daily(cred):
     cookie = dict()
     cookie['name'] = 'GMKT.FRONT.SG'
     cookie['url'] = 'https://.qoo10.sg'
+    cookie['value'] = cred.strip()
 
-    browser = await launch()
+    browser = await launch({'executablePath': '/usr/bin/chromium-browser'})
     page = await browser.newPage()
 
-    for cred in credentials:
-        cookie['value'] = cred.strip()
-        await page.setCookie(cookie)
-        await page.goto('https://www.qoo10.sg/gmkt.inc/Event/qchance.aspx')
+    await page.setCookie(cookie)
+    await page.goto('https://www.qoo10.sg/gmkt.inc/Event/qchance.aspx')
 
-        output = await page.evaluate('''() => {
-            window.frames[0].Attendance.Apply()
-            window.frames[0].frames[0].RouletteQ.Apply()
+    output = await page.evaluate('''() => {
+        window.frames[0].Attendance.Apply()
+        window.frames[0].frames[0].RouletteQ.Apply()
 
-            return {
-                name: document.querySelector('a.name').innerHTML,
-            }
-        }''')
+        return {
+            name: document.querySelector('a.name').innerHTML,
+        }
+    }''')
 
-        print(output)
+    print(output)
     
     await browser.close()
 
 async def main():
     credentials = await get_credentials()
-    await do_daily(credentials)
+    
+    for cred in credentials:
+        await do_daily(cred)
 
 asyncio.get_event_loop().run_until_complete(main())
 

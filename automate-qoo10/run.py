115e1f41c7a9cbe73acb6f68b31daa1ba70c29e2
Yong Hao TENG <yonghao.teng@gmail.com>
2019-04-22 14:17:45 +0800

    Use system's chromium for arm devices

--- automate-qoo10/run.py
+++ automate-qoo10/run.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import aiofiles
+import platform
 from pyppeteer import launch
 
 async def get_credentials():
@@ -10,13 +11,17 @@ async def get_credentials():
 
     return credentials
 
-async def do_daily(cred):
+async def do_daily(credential):
     cookie = dict()
     cookie['name'] = 'GMKT.FRONT.SG'
     cookie['url'] = 'https://.qoo10.sg'
-    cookie['value'] = cred.strip()
+    cookie['value'] = credential.strip()
 
-    browser = await launch({'executablePath': '/usr/bin/chromium-browser'})
+    opts = {}
+    if platform.machine() == 'armv7l':
+        opts = {'executablePath': '/usr/bin/chromium-browser'}
+
+    browser = await launch(opts)
     page = await browser.newPage()
 
     await page.setCookie(cookie)
@@ -24,6 +29,8 @@ async def do_daily(cred):
 
     output = await page.evaluate('''() => {
         window.frames[0].Attendance.Apply()
+        window.frames[0].Qticket.ChangeQticket()
+        window.frames[0].frames[0].RouletteQ.Apply()
         window.frames[0].frames[0].RouletteQ.Apply()
 
         return {
@@ -32,14 +39,14 @@ async def do_daily(cred):
     }''')
 
     print(output)
-    
+
     await browser.close()
 
 async def main():
     credentials = await get_credentials()
-    
-    for cred in credentials:
-        await do_daily(cred)
+
+    for credential in credentials:
+        await do_daily(credential)
 
 asyncio.get_event_loop().run_until_complete(main())
 

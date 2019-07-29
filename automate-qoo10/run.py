5dbaad5f6bdaa0d8cc1bc097b0348b219a00d452
Yong Hao TENG <yonghao.teng@gmail.com>
2019-07-29 22:02:04 +0800

    Add telegram notify when token expires

--- automate-qoo10/run.py
+++ automate-qoo10/run.py
@@ -3,6 +3,7 @@
 import asyncio
 import aiofiles
 import platform
+import os
 from pyppeteer import launch
 
 async def get_credentials():
@@ -17,34 +18,41 @@ async def do_daily(credential):
     cookie['url'] = 'https://.qoo10.sg'
     cookie['value'] = credential.strip()
 
-    opts = {}
+    opts = {'headless': True}
     if platform.machine() == 'armv7l':
-        opts = {'executablePath': '/usr/bin/chromium-browser'}
+        opts['executablePath'] = '/usr/bin/chromium-browser'
 
     browser = await launch(opts)
     page = await browser.newPage()
 
     await page.setCookie(cookie)
-    await page.goto('https://www.qoo10.sg/gmkt.inc/Event/qchance.aspx')
+    await page.goto('https://www.qoo10.sg/gmkt.inc/Event/Qchance.aspx#reward')
+    await asyncio.sleep(3)
 
-    output = await page.evaluate('''() => {
-        window.frames[0].Attendance.Apply()
-        window.frames[0].Qticket.ChangeQticket()
-        window.frames[0].frames[0].RouletteQ.Apply()
-        window.frames[0].frames[0].RouletteQ.Apply()
+    try:
+        output = await page.evaluate('''() => {
+            window.frames[0].Attendance.Apply()
+            window.frames[0].Qticket.ChangeQticket()
+            window.frames[0].frames[0].RouletteQ.Apply()
+            window.frames[0].frames[0].RouletteQ.Apply()
 
-        return {
-            name: document.querySelector('a.name').innerHTML,
-        }
-    }''')
-
-    print(output)
-
+            return {
+                name: document.querySelector('a.name').innerHTML
+            }
+        }''')
+    
+        print(output)
+    except TypeError:
+        notify(credential[:8] + "... has expired.")
+    
     await browser.close()
+
+def notify(mesg):
+    if os.path.exists('telegram.conf'):
+        os.system('/usr/bin/printf \"{}\" | /usr/local/bin/telegram-send --format markdown --config telegram.conf --stdin'.format(mesg))
 
 async def main():
     credentials = await get_credentials()
-
     for credential in credentials:
         await do_daily(credential)
 

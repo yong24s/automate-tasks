0e8e70d69fb86b63adc08cc13a8cb5e7fba180b2
Yong Hao TENG <witchoftheholynight@gmail.com>
2019-12-28 18:59:23 +0800

    fitbit: include various exercises

--- automate-fitbit/run.py
+++ automate-fitbit/run.py
@@ -10,8 +10,22 @@ def get_today_date():
 
 def log_running_activity(date):
     headers = {'Authorization': 'Bearer ' + credentials.ACCESS_TOKEN}
-    data = {'activityId': 90009, 'distance': 5, 'startTime': '06:00:00', 'durationMillis': 1*60*60*1000, 'date': date}
 
+    # Run
+    # data = {'activityId': 90009, 'distance': 5, 'startTime': '06:00:00', 'durationMillis': 1*60*60*1000, 'date': date}
+
+    # Rope Jumping
+    # data = {'activityId': 15550, 'startTime': '08:30:00', 'durationMillis': 1*60*60*1000, 'date': date}
+    
+    # Run 8mph (7.5 min/mile)
+    # data = {'activityId': 12090, 'distance': 10, 'startTime': '08:30:00', 'durationMillis': 1*60*60*1000, 'date': date}
+
+    # Walk with speed 90013; Walk without speed 17160
+    # data = {'activityId': 17160, 'startTime': '06:00:00', 'durationMillis': 2*60*60*1000, 'date': date}
+    
+#    data = {'activityId': 90013, 'distance':10, 'startTime': '06:00:00', 'durationMillis': 1*60*60*1000, 'date': date}
+
+    data = {'activityId': 90009, 'distance': 5, 'startTime': '10:00:00', 'durationMillis': '299000', 'date': date}
     r = requests.post('https://api.fitbit.com/1/user/{}/activities.json'.format(credentials.USER_ID), headers=headers, data=data)
     print(r.status_code)
     print(r.text)

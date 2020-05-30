44e96f702283fa688324ed78569d413a2be4f339
Yong Hao TENG <yonghao.teng@gmail.com>
2020-05-30 20:16:07 +0800

    automate-fitbit: change activity

--- automate-fitbit/run.py
+++ automate-fitbit/run.py
@@ -11,24 +11,25 @@ def get_today_date():
 def log_running_activity(date):
     headers = {'Authorization': 'Bearer ' + credentials.ACCESS_TOKEN}
 
-    # Run
-    # data = {'activityId': 90009, 'distance': 5, 'startTime': '06:00:00', 'durationMillis': 1*60*60*1000, 'date': date}
-
-    # Rope Jumping
-    # data = {'activityId': 15550, 'startTime': '08:30:00', 'durationMillis': 1*60*60*1000, 'date': date}
-    
-    # Run 8mph (7.5 min/mile)
-    # data = {'activityId': 12090, 'distance': 10, 'startTime': '08:30:00', 'durationMillis': 1*60*60*1000, 'date': date}
-
-    # Walk with speed 90013; Walk without speed 17160
-    # data = {'activityId': 17160, 'startTime': '06:00:00', 'durationMillis': 2*60*60*1000, 'date': date}
-    
-#    data = {'activityId': 90013, 'distance':10, 'startTime': '06:00:00', 'durationMillis': 1*60*60*1000, 'date': date}
-
-    data = {'activityId': 90009, 'distance': 5, 'startTime': '10:00:00', 'durationMillis': '299000', 'date': date}
+    data = {
+    	'activityId': 12030, # Run acivity
+		'activityParentId': 90009,
+		'activityParentName': 'Run',
+		'calories': 1087,
+		'description': 'Running - 5 mph (12 min/mile)',
+		'distance': 15,
+		'durationMillis': 7200000,
+        'date': date,
+		'hasStartTime': True,
+		'isFavorite': False,
+		'name': 'Run',
+		'startTime': '12:00',
+		'steps': 12711
+	}
+  
     r = requests.post('https://api.fitbit.com/1/user/{}/activities.json'.format(credentials.USER_ID), headers=headers, data=data)
     print(r.status_code)
     print(r.text)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     log_running_activity(get_today_date())
